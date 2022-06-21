1651a8850afe8a119acbe83a837ceafd0209e408
Grant Nations <nationsgrant@gmail.com>
2022-06-20 21:45:21 -0600

    first commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,9 @@
+const { network } = require("hardhat");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+
+    // when using localhost or hardhat network, use mock data
+};
